a7667f78c6a49620547015ed41520cf5d2be28a8
flycool <flycoolcool@163.com>
2023-01-02 16:50:18 +0800

    add accompanist-webview to show website

--- app/src/main/java/com/example/maxnews/ui/navigation/ArticleScreen.kt
+++ app/src/main/java/com/example/maxnews/ui/navigation/ArticleScreen.kt
@@ -1,10 +1,14 @@
 package com.example.maxnews.ui.navigation
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,8 +17,13 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.example.maxnews.R
 import com.example.maxnews.data.model.Article
 import com.example.maxnews.ui.NewsViewModel
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.LoadingState
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "SetJavaScriptEnabled")
 @Composable
 fun ArticleScreen(
     article: Article?,
@@ -49,15 +58,46 @@ fun ArticleScreen(
             }
         },
     ) {
-        AndroidView(
-            factory = {
-                WebView(it).apply {
-                    webViewClient = WebViewClient()
-                    article?.let {
-                        loadUrl(article.url)
+
+        Column (modifier = Modifier.fillMaxSize()) {
+            val state = rememberWebViewState(url = article!!.url)
+            val loadingState = state.loadingState
+
+            if (loadingState is LoadingState.Loading) {
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth(),
+                    progress = loadingState.progress
+                )
+            }
+
+            val webViewClient = remember {
+                object : AccompanistWebViewClient() {
+                    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                        super.onPageStarted(view, url, favicon)
+                        Log.d("ArticleScreen", "onPageStarted: $url")
                     }
                 }
-            },
-        )
+            }
+
+            WebView(
+                state = state,
+                onCreated = {
+                    it.settings.javaScriptEnabled = true
+                },
+                client = webViewClient
+            )
+
+            /*AndroidView(
+                factory = {
+                    WebView(it).apply {
+                        webViewClient = WebViewClient()
+                        article?.let {
+                            loadUrl(article.url)
+                        }
+                    }
+                },
+            )*/
+        }
+
     }
 }
